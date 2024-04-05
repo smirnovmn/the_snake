119d8d917e44681339a4337b21ad9513dccd8d32
Михаил Смирнов <rucka.1@yandex.ru>
2024-04-05 14:09:22 +0500

    test

--- the_snake.py
+++ the_snake.py
@@ -2,47 +2,42 @@ from random import choice, randint
 
 import pygame
 
-# Инициализация PyGame:
 pygame.init()
 
-# Константы для размеров поля и сетки:
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
-# Направления движения:
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Цвет фона - черный:
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 
-# Цвет границы ячейки
 BORDER_COLOR = (93, 216, 228)
 
-# Цвет яблока
 APPLE_COLOR = (255, 0, 0)
 
-# Цвет змейки
 SNAKE_COLOR = (0, 255, 0)
 
-# Скорость движения змейки:
 SPEED = 20
 
-# Настройка игрового окна:
+DIRECTIONS = {
+    'up': pygame.K_UP,
+    'down': pygame.K_DOWN,
+    'left': pygame.K_LEFT,
+    'right': pygame.K_RIGHT
+}
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
-# Заголовок окна игрового поля:
 pygame.display.set_caption('Змейка')
 
-# Настройка времени:
 clock = pygame.time.Clock()
 
 
-# Тут опишите все классы игры.
 class GameObject:
     """Базовый класс"""
 
@@ -125,7 +120,6 @@ class Snake(GameObject):
     def draw(self):
         """Метод для отрисовки"""
         for position in self.positions:
-            # Используйте конструктор pygame.Rect для создания прямоугольника
             rect = pygame.Rect(position[0], position[1], GRID_SIZE, GRID_SIZE)
             pygame.draw.rect(screen, self.body_color, rect)
             pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
@@ -142,29 +136,26 @@ def handle_keys(game_object):
             pygame.quit()
             raise SystemExit
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and game_object.direction != DOWN:
+            if event.key == DIRECTIONS['up'] and game_object.direction != DOWN:
                 game_object.next_direction = UP
-            elif event.key == pygame.K_DOWN and game_object.direction != UP:
+            elif event.key == DIRECTIONS['down'] and game_object.direction != UP:
                 game_object.next_direction = DOWN
-            elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
+            elif event.key == DIRECTIONS['left'] and game_object.direction != RIGHT:
                 game_object.next_direction = LEFT
-            elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
+            elif event.key == DIRECTIONS['right'] and game_object.direction != LEFT:
                 game_object.next_direction = RIGHT
 
 
 def main():
     """Основная логика игры"""
-    # Тут нужно создать экземпляры классов.
     snake = Snake()
     apple = Apple()
-    apple.draw()
-    snake = Snake()
+    apple.draw()    
     snake.draw()
 
     while True:
         clock.tick(SPEED)
 
-        # Обработка событий клавиш
         handle_keys(snake)
 
         for event in pygame.event.get():
@@ -173,11 +164,10 @@ def main():
                 break
 
         snake.update_direction()
-        snake.move(apple)  # Вот здесь должен быть вызов метода move()
+        snake.move(apple)
 
-        # Проверка на наличие яблока на территории змеи
         if apple.position in snake.positions:
-            apple.randomize_position()  # Перерисовка яблока
+            apple.randomize_position()
 
         screen.fill(BOARD_BACKGROUND_COLOR)
         snake.draw()
